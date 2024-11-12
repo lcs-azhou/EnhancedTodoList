937a70399e522d0e09bba67b69e95272d91ed3a6
Ansheng Zhou <azhou@lcs.on.ca>
2024-11-12 11:54:34 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by Ansheng Zhou on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
